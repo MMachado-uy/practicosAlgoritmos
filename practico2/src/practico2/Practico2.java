773fb78e13e417a789ff471c13a897d0487d66ed
Mauricio Machado <mauricio.machado@modelit.xyz>
2019-09-04 21:31:23 -0300

    Practico 2

---
+++ practico2/src/practico2/Practico2.java
@@ -0,0 +1,54 @@
+package practico2;
+
+public class Practico2 {
+
+	public static void main(String[] args) {
+		ej3(8, 4);
+		ej5(30,14);
+		
+		System.out.println(esCapicua(1235321));
+	}
+
+	private static void ej3(int a, int b) {
+		int cont = 0;
+
+		while (a >= b) {
+			cont++;
+			a -= b;
+		}
+		
+		System.out.println("Cociente: " + cont);
+		System.out.println("Resto: " + a);
+	}
+	
+	private static int ej4(int a, int b) {
+		int max = (a >= b) ? a : b;
+		int min = (a <= b) ? a : b;
+		
+		while (min > 0) {
+			int aux = max;
+			max = min;
+			min = aux % min;
+		}
+		
+		return max;
+	}
+	
+	private static void ej5(int num, int den) {
+		int mcd = ej4(num, den);
+		System.out.println("" + num/mcd + ((den/mcd > 1) ? "/" + den/mcd : ""));
+	}
+	
+	private static boolean esCapicua(int num) {
+		String str = "" + num;
+		boolean esCapicua = false;
+		int i = 0;
+		
+		while (i < str.length() / 2 && !esCapicua) {
+			esCapicua = str.charAt(i) == str.charAt(str.length() - 1 - i);
+			i++;
+		}
+		
+		return esCapicua;
+	}
+}
